e10398b25d9feeaa565a71858ef8d182fb756fa0
fflachat <florent.flachat@gmail.com>
2022-05-12 14:14:41 +0200

    create routes

--- beecolo/src/App.js
+++ beecolo/src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Conseils from './pages/Conseils';
+import Simulateur from './pages/Simulateur';
+import Contacts from './pages/Contacts';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/conseils" element={<Conseils />} />
+        <Route path="/simulateur" element={<Simulateur />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
