b1dcdf6a7dde28cdd8fb39cc3ddbd06d7f1297bc
fflachat <florent.flachat@gmail.com>
2022-05-12 18:03:55 +0200

    add one question

--- beecolo/src/pages/Simulateur.js
+++ beecolo/src/pages/Simulateur.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import MapEspaceRenov from '../components/MapEspaceRenov/MapEspaceRenov';
 import ChoixTravaux from '../components/simulateur/ChoixTravaux';
 import FoyerInfos from '../components/simulateur/FoyerInfos';
 import Logement from '../components/simulateur/Logement';
@@ -11,7 +10,7 @@ export default function Simulateur() {
   const [questionIndex, setQuestionIndex] = useState(1);
 
   const switchQuestion = () => {
-    if (questionIndex < 4) setQuestionIndex(questionIndex + 1);
+    if (questionIndex < 5) setQuestionIndex(questionIndex + 1);
     else setQuestionIndex(1);
   };
 
@@ -25,8 +24,9 @@ export default function Simulateur() {
       <div className="questions_card">
         {questionIndex === 1 ? <Logement /> : null}
         {questionIndex === 2 ? <LogementInfos /> : null}
-        {questionIndex === 3 ? <ChoixTravaux /> : null}
-        {questionIndex === 4 ? <SimulateurResult /> : null}
+        {questionIndex === 3 ? <FoyerInfos /> : null}
+        {questionIndex === 4 ? <ChoixTravaux /> : null}
+        {questionIndex === 5 ? <SimulateurResult /> : null}
       </div>
       <button type="button" onClick={switchQuestion}>
         Suivant
